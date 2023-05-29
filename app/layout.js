44daac711ae7ff310657dcdcad09cdda0bb7cade
tahir <tahiraydin06@gmail.com>
2023-05-29 15:11:52 +0300

    next movies

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import { Roboto } from "next/font/google";
+
+import "@/styles/reset.css";
+import "@/styles/global.css";
+
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={roboto.className}>
+      <body className="container">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
